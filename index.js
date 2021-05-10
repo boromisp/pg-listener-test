03c6bad96ab2df723c685df15720fc49377507b0
Peter Boromissza <2242020+boromisp@users.noreply.github.com>
2021-05-10 09:18:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  query_timeout: 1000, // Ping query timeout
+});
+
+const HEARTBEAT_TIMEOUT = 5000;
+
+async function ping(client) {
+  try {
+    await client.query(";");
+  } catch (error) {
+    console.error("Ping error", error);
+
+    try {
+      await client.end();
+    } catch (error) {
+      console.log("Disconnect error", err);
+    }
+  }
+}
+
+async function main() {
+  await client.connect();
+
+  let pingTimeout = setTimeout(() => ping(client), HEARTBEAT_TIMEOUT);
+
+  client.on("notification", (n) => {
+    console.log(`[${n.processId}] ${n.channel}: ${n.payload || ""}`);
+
+    clearTimeout(pingTimeout);
+    pingTimeout = setTimeout(() => ping(client), HEARTBEAT_TIMEOUT);
+  });
+
+  client.once("end", () => clearTimeout(pingTimeout));
+
+  await client.query("LISTEN foo");
+}
+
+main();
